32bd489b4689e49c1b9fe8df5834d802d939c4f9
vb3519 <vb415@bk.ru>
2025-09-19 22:31:28 +0300

    стартовая сборка

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const greeting: string = 'Hello from React App ^_^';
+
+  console.log('Greeting:', greeting);
+
+  return (
+    <div className="p-2 text-bold text-amber-500 text-center">
+      Hello from React ^_^
+    </div>
+  );
+};
+
+export default App;

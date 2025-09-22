e0317733e3d979a66682db3ece08f21ba29a76d0
vb3519 <vb415@bk.ru>
2025-09-22 23:22:31 +0300

    отдельный компонент формы для авторизации

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,9 @@
-// Ui:
-import CustomInput from '../shared/ui/CustomInput';
-import CustomButton from '../shared/ui/CustomButton';
+import UserAuthForm from './components/UserAuthForm';
 
 const App = () => {
   return (
-    <div className="p-4 flex flex-col gap-2 items-center justify-center">
-      <CustomInput
-        className="w-full max-w-lg border-2 border-blue-300 rounded-md"
-        placeholder="Укажите логин..."
-      />
-
-      <CustomInput
-        className="w-full max-w-lg border-2 border-blue-300 rounded-md"
-        placeholder="Укажите пароль..."
-      />
-
-      <CustomButton className="bg-blue-500 text-white transition duration-200 hover:bg-blue-600">
-        Зарегистрироваться
-      </CustomButton>
+    <div className="p-2 h-screen flex items-center justify-center bg-[#232529]">
+      <UserAuthForm />
     </div>
   );
 };
